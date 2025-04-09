758858b9de9926675c409993c84b7b301da541a7
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2025-04-09 11:42:25 +0530

    Update ImgToUrlRoutes.js

--- Routes/ImgToUrlRoutes.js
+++ Routes/ImgToUrlRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller and middleware
-const { uploadfile, uploadMiddleware } = require("../controllers/ImgToUrl");
+const { uploadfile, uploadMiddleware } = require("../Controllers/ImgToUrl");
 const validateToken = require("../Middlewares/validateToken");
 
 // Route: Upload image to Cloudinary
